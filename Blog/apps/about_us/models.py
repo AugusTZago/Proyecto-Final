b675e82141738f4dad0ef0e268ae99890ff4f27c
NiktoARG <nicolastzago@gmail.com>
2024-10-16 20:23:52 -0300

    apps

--- Blog/apps/about_us/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Nosotros(models.Model):
-    nombre = models.CharField(max_length=50)
-    edad = models.IntegerField(max_length=2)
-    
--- Blog/apps/usuario/apps.py
+++ Blog/apps/usuario/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AboutUsConfig(AppConfig):
+class UsuarioConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.about_us'
+    name = 'usuario'
